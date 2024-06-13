0f750d621934ddf4409b601a04aaa23a1d23cb37
aomkhamphaung <akhamphaung@gmail.com>
2024-06-13 22:35:09 +0630

    feat: create book

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
 import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/Book.js";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(200).send("Welcome");
+});
+
+// Create book
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishedYear) {
+      return res.status(400).send({ message: "Creating book failed!" });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishedYear: req.body.publishedYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send({ book });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
