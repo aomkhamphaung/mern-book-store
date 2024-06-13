d73b615cea72778e120ce786e6e667232bb4569e
aomkhamphaung <akhamphaung@gmail.com>
2024-06-13 21:53:31 +0630

    connect: mongodb and mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5000;
+
+export const mongodbURL = "mongodb+srv://admin:password010203@cluster0.cou0xi1.mongodb.net/book-store?retryWrites=true&w=majority&appName=Cluster0";
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import {PORT} from "./config.js";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(200).send("Welcome");
-})
-
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(200).send("Welcome");
 });
+
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App is running on port ${PORT}`);
+    });
+    console.log(`Connected to database`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
