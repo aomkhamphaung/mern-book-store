4302691a7195cef40ce20825e811d0405e56c5d9
aomkhamphaung <akhamphaung@gmail.com>
2024-06-14 12:00:45 +0630

    setup: cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,20 @@ import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+
+// Middleware for CORS Policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+  })
+)
 
 app.get("/", (req, res) => {
   console.log(req);
